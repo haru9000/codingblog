c4a86862e4da951d129914e96049466dee0edf16
haru9000 <56687112+haru9000@users.noreply.github.com>
2023-04-29 14:15:05 +0900

    Update preNote.js

--- src/preNote/preNote.js
+++ src/preNote/preNote.js
@@ -1,20 +1,22 @@
-export function preNodePopup() {
-    // 「Copy code」ボタンをクリックしたときに、preタグの中身をクリップボードにコピーする
-    var preElements = document.querySelectorAll('pre.note');
-    for (var i = 0; i < preElements.length; i++) {
-        preElements[i].addEventListener('click', function (event) {
-            var code = event.target.innerText;
-            navigator.clipboard.writeText(code).then(function () {
-                var popup = document.createElement('div');
-                popup.innerHTML = 'コードがクリップボードにコピーされました';
-                popup.classList.add('popup');
-                popup.classList.add('visible');
-                document.body.appendChild(popup);
-                setTimeout(function () {
-                    popup.classList.remove('visible');
-                }, 1000);
-            }, function () {
+export default class preNote {
+    function preNodePopup() {
+        // 「Copy code」ボタンをクリックしたときに、preタグの中身をクリップボードにコピーする
+        var preElements = document.querySelectorAll('pre.note');
+        for (var i = 0; i < preElements.length; i++) {
+            preElements[i].addEventListener('click', function (event) {
+                var code = event.target.innerText;
+                navigator.clipboard.writeText(code).then(function () {
+                    var popup = document.createElement('div');
+                    popup.innerHTML = 'コードがクリップボードにコピーされました';
+                    popup.classList.add('popup');
+                    popup.classList.add('visible');
+                    document.body.appendChild(popup);
+                    setTimeout(function () {
+                        popup.classList.remove('visible');
+                    }, 1000);
+                }, function () {
+                });
             });
-        });
+        }
     }
 }
