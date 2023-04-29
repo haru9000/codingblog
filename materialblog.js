ecf43821a427f4b68bf7744090d6f18381b6eb6d
haru9000 <56687112+haru9000@users.noreply.github.com>
2023-04-29 14:26:26 +0900

    Update materialblog.js

--- materialblog.js
+++ materialblog.js
@@ -1,6 +1,11 @@
 import preNote from 'https://haru9000.github.io/codingblog/src/preNote/preNote.js';
 
+// preNoteインスタンスを作成して、preNodePopup()メソッドを呼び出す
+const preNoteInstance = new preNote();
+preNoteInstance.preNodePopup();
+console.log('1');
 // ロード時の処理
 window.addEventListener('load', function() {
-    preNote.preNote.preNodePopup();
+    preNoteInstance.preNodePopup();
+    console.log('2');
 });
