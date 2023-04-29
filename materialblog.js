3aa878c54a4be05ea26487de4b3f024ce6197bef
haru9000 <56687112+haru9000@users.noreply.github.com>
2023-04-29 13:37:30 +0900

    Update materialblog.js

--- materialblog.js
+++ materialblog.js
@@ -1,4 +1,4 @@
-import preNote from './src/preNote/preNote.js';
+import preNote from 'https://haru9000.github.io/codingblog/src/preNote/preNote.js';
 
 // ロード時の処理
 window.addEventListener('load', function() {
