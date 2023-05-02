e225f327811d9dc3b39d5c4dc180599463070cf9
haru9000 <56687112+haru9000@users.noreply.github.com>
2023-05-02 12:28:28 +0900

    Update starRating.js

--- src/starRating/starRating.js
+++ src/starRating/starRating.js
@@ -1,15 +1,24 @@
-const starRating = document.querySelector('.star-rating');
-const stars = starRating.querySelectorAll('.fa-star');
-const rating = starRating.getAttribute('data-rating');
+export default class starRating {
+  constructor() {
+  }
 
-function setRating(rating) {
-  stars.forEach((star, index) => {
-    if (index < rating) {
-      star.classList.add('active');
-    } else {
-      star.classList.remove('active');
-    }
-  });
+  setRating(starRating) {
+    const stars = starRating.querySelectorAll('.fa-star');
+    const rating = starRating.getAttribute('data-rating');
+
+    stars.forEach((star, index) => {
+      if (index < rating) {
+        star.classList.add('active');
+      } else {
+        star.classList.remove('active');
+      }
+    });
+  }
+
+  setAllRating() {
+    const starRatings = document.querySelectorAll('.star-rating');
+    starRatings.forEach((starRating) => {
+      this.setRating(starRating);
+    });
+  }
 }
-
-setRating(parseFloat(rating));
