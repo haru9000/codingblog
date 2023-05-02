316b12022c2fcf3c492f62c65d7b699feecee793
haru9000 <56687112+haru9000@users.noreply.github.com>
2023-05-02 14:51:51 +0900

    Create harusdia.js

---
+++ harusdia.js
@@ -0,0 +1,11 @@
+import preNote from 'https://haru9000.github.io/codingblog/src/preNote/preNote.js';
+
+// preNoteインスタンスを作成して、preNodePopup()メソッドを呼び出す
+const preNoteInstance = new preNote();
+preNoteInstance.preNodePopup();
+
+import starRating from 'https://haru9000.github.io/codingblog/src/starRating/starRating.js';
+
+// starRatingインスタンスを作成して、preNodePopup()メソッドを呼び出す
+const starRatingInstance = new starRating();
+starRatingInstance.setAllRating();
