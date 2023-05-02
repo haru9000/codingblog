21ee7749d6a8d15058bdeec65e8714f3c1ab07d4
haru9000 <56687112+haru9000@users.noreply.github.com>
2023-05-02 12:51:52 +0900

    Update starRating.js

--- src/starRating/starRating.js
+++ src/starRating/starRating.js
@@ -3,16 +3,21 @@ export default class starRating {
   }
 
   setRating(starRating) {
-    const stars = starRating.querySelectorAll('.fa-star');
-    const rating = starRating.getAttribute('data-rating');
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 !== 0;
+    const halfStar = stars[fullStars];
 
     stars.forEach((star, index) => {
-      if (index < Math.ceil(rating)) {
+      if (index < fullStars) {
         star.classList.add('active');
       } else {
         star.classList.remove('active');
       }
     });
+
+    if (hasHalfStar) {
+      halfStar.classList.add('active');
+    }
   }
 
   setAllRating() {
